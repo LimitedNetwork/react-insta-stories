9c258f54b2dacbdb8569c58917647826c9241480
Dmitry Rudiuk <dmitry.rudiuk@gmail.com>
2024-07-25 08:24:23 +0300

    removed "prepare" script to fix fail on CI; removed dist folder from gitignore

---
+++ dist/index.d.ts
@@ -0,0 +1,16 @@
+import React from 'react';
+import { ReactInstaStoriesProps, Story } from './interfaces';
+declare const ReactInstaStories: ({ width, height, defaultInterval, preloadCount, ...props }: ReactInstaStoriesProps) => JSX.Element;
+export declare const WithHeader: React.FC<React.PropsWithChildren<{
+    story: Story;
+    globalHeader: Function;
+}>>;
+export declare const WithSeeMore: React.FC<React.PropsWithChildren<{
+    story: Story;
+    action: import("./interfaces").Action;
+    customCollapsed?: React.ComponentType<{
+        toggleMore: (show: boolean) => void;
+        action: import("./interfaces").Action;
+    }>;
+}>>;
+export default ReactInstaStories;
